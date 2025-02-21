e2ef8014a8228d2e032f3ec7c142beefca50989c
pons <157600585+fnscbrn@users.noreply.github.com>
2025-02-21 08:05:45 +0800

    Add files via upload

---
+++ reverse3.py
@@ -0,0 +1,16 @@
+#triangle
+rows = 5
+
+for i in range(rows, 0, -1):
+    print(' ' * (rows - i) + '*' * (2 * i - 1))
+
+print(' ')
+
+#diamond
+rows = 5
+
+for i in range(1, rows + 1):
+    print(' ' * (rows - i) + '*' * (2 * i - 1))
+
+for i in range(rows - 1, 0, -1):
+    print(' ' * (rows - i) + '*' * (2 * i - 1))
